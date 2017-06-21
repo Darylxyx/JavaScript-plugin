6022d0b3f08d12fed76d74ff68d9c10273048946
Darylxyx <921218060@qq.com>
2017-06-21 10:26:46 +0800

    update

--- base/base.js
+++ base/base.js
@@ -2,8 +2,31 @@
 
 	function Base() {}
 
-	Base.prototype.queryParams = function() {
+	Base.prototype.server = function(Ajax, data, url, callback, type, dataType) { //Server方法
+		Ajax({
+			data: data || {},
+			type: type || 'get',
+			url: url,
+			dataType: dataType || 'json',
+			done: (res) => {
+				callback && callback(res);
+			}
+		});
+	};
 
+	Base.prototype.queryParams = function() { //获取查询字符串参数
+		var search = location.search,
+			theRequest = {};
+		if (search.indexOf('?') < 0) {
+			return;
+		}
+		search = search.substr(1);
+		var paramArr = search.split('&'),
+			max = paramArr.length;
+		for (var i = 0; i < max; i ++) {
+			theRequest[paramArr[i].split('=')[0]] = unescape(paramArr[i].split('=')[1]);
+		}
+		return theRequest;
 	};
 
 	var base = new Base();
@@ -13,10 +36,10 @@
 })(window, document);
 
 if (typeof module !== 'undefined') {
-	module.exports = window.Ajax;
+	module.exports = window.base;
 } else if (typeof define === 'function' && define.amd) {
     define([], function () {
         'use strict';
-        return window.Ajax;
+        return window.base;
     });
 }
