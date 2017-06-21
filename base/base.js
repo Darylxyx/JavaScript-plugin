34a63fafa3f63ed99c280b35e70da637800be9df
Darylxyx <921218060@qq.com>
2017-06-21 14:32:27 +0800

    update

--- base/base.js
+++ base/base.js
@@ -2,16 +2,21 @@
 
 	function Base() {}
 
-	Base.prototype.server = function(Ajax, data, url, callback, type, dataType) { //Server方法
-		Ajax({
-			data: data || {},
-			type: type || 'get',
-			url: url,
-			dataType: dataType || 'json',
-			done: (res) => {
-				callback && callback(res);
-			}
-		});
+	Base.prototype.server = function(Ajax) { //Server方法
+		
+		var Server = Ajax;
+
+		return function(data, url, callback, type, dataType) {
+			Server({
+				data: data || {},
+				type: type || 'get',
+				url: url,
+				dataType: dataType || 'json',
+				done: (res) => {
+					callback && callback(res);
+				}
+			});
+		}
 	};
 
 	Base.prototype.queryParams = function() { //获取查询字符串参数
